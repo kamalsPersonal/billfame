f4bfa4071ede0aa6c8e6668fd5ced2c87ff13a9a
Kamal Khatwani <kamalkhatwani69@gmail.com>
2018-04-13 00:49:30 +0530

    Basic homepage with header and footer

--- client/App.js
+++ client/App.js
@@ -1,23 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+    Container,
+    Header,
+    Content,
+    Text,
+    Button,
+    Icon,
+    Left,
+    Body,
+    Right,
+    Title,
+    Footer,
+    FooterTab
+} from 'native-base';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+
+export default class AnatomyExample extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {loading: true};
+    }
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+        this.setState({loading: false});
+    }
+
+
+    render() {
+
+        if (!this.state.loading) {
+            return (
+                <Container>
+                    <Header>
+                        <Left>
+                            <Button transparent>
+                                <Icon name='menu'/>
+                            </Button>
+                        </Left>
+                        <Body>
+                        <Title>Header</Title>
+                        </Body>
+                        <Right />
+                    </Header>
+                    <Content>
+                        <Text>
+                            This is Content Section
+                        </Text>
+                    </Content>
+                    <Footer>
+                        <FooterTab>
+                            <Button full>
+                                <Text>Footer</Text>
+                            </Button>
+                        </FooterTab>
+                    </Footer>
+                </Container>
+            );
+        }
+        else
+            return null;
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
