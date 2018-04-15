fd46adfd77fe6ef1d97735a8854e13dbee868e93
Kamal Khatwani <kamalkhatwani69@gmail.com>
2018-04-15 23:08:00 +0530

    Added the "you" card, fixed image height

--- client/App.js
+++ client/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, View, Dimensions} from 'react-native';
+import Swiper from 'react-native-deck-swiper';
 import {
     Container,
     Header,
@@ -16,6 +17,7 @@ import {
     Title,
     Footer,
     FooterTab,
+    DeckSwiper,
     H1, H2, H3,
     Col, Row, Grid
 } from 'native-base';
@@ -37,37 +39,52 @@ export default class AnatomyExample extends Component {
     }
 
     render() {
-
+        var {height, width} = Dimensions.get('window');
         if (!this.state.loading) {
             return (
                 <Container>
-                    <Card transparent>
-                        <CardItem cardBody>
-                            <Image source={require('./images/bill_gates.jpg')}/>
-                        </CardItem>
-                        <CardItem style={{backgroundColor: '#463d3d'}}>
-                            <H1 style={{color: 'white'}}>Bill Gates<Text style={{color: 'white'}}> (United
-                                States of
-                                America)</Text></H1>
+                    <Swiper
+                        cards={cards}
+                        horizontalSwipe={false}
+                        infinite={true}
+                        stackAnimationTension={100}
+                        showSecondCard={false}
+                        goBackToPreviousCardOnSwipeBottom={true}
+                        cardVerticalMargin={0}
+                        cardHorizontalMargin={0}
+                        backgroundColor="white"
+                        renderCard={(item) => {
+                            return (
+                                <View transparent style={{flex: 1, justifyContent: 'center'}}>
+                                    <View cardBody style={{flex: 5}}>
+                                        <Image source={item.image} style={{height: height, width: width}}/>
+                                    </View>
+                                    <View style={{backgroundColor: '#463d3d', padding: 20, flex: 1, opacity: 0.7, alignItems: 'center'}}>
+                                        <H1 style={{color: 'white'}}>{item.name} <Text style={{color: 'white'}}>
+                                            ({item.country})</Text></H1>
+                                    </View>
+                                    <View style={{backgroundColor: '#463d3d', padding: 20, flex: 1, opacity: 0.7, alignItems: 'center'}}>
+                                        <H3 style={{color: 'white'}}>Global Rank: {item.globalRank}th {"\n"}National
+                                            Rank: {item.nationalRank}th</H3>
+                                    </View>
+                                    <View style={{backgroundColor: '#463d3d', padding: 20, flex: 1, opacity: 0.7, alignItems: 'center'}}>
+                                        <H3 style={{color: 'white'}}>Total worth: {item.totalWorth} USD</H3>
+                                    </View>
+                                    <View style={{backgroundColor: '#776a67b3', padding: 20, flex: 1, opacity: 0.7, alignItems: 'center'}}>
+                                        <Text style={{color: 'white'}}>"{item.quote}"</Text>
+                                    </View>
+                                </View>
+                            )
+                        }}>
+                    </Swiper>
 
-                        </CardItem>
-                        <CardItem style={{backgroundColor: '#776a67'}}>
-                            <H3 style={{color: 'white'}}>Global Rank: 6th {"\n"}National Rank: 6th</H3>
-                        </CardItem>
-                        <CardItem style={{backgroundColor: '#776a67'}}>
-                            <H3 style={{color: 'white'}}>Total worth: 456,004,249 USD</H3>
-                        </CardItem>
-                        <CardItem style={{backgroundColor: '#776a67b3', flexGrow: 1}}>
-                            <Text style={{color: 'white'}}>"If you're born poor, it's not your mistake. But if you die
-                                poor, it's your mistake!"</Text>
-                        </CardItem>
-                    </Card>
-                    <Fab active={this.state.active}
+
+                   {/* <Fab active={this.state.active}
                          style={{backgroundColor: '#E65100'}}
                          position="bottomRight"
                          onPress={() => this.setState({active: !this.state.active})}>
                         <Icon name="add"/>
-                    </Fab>
+                    </Fab>*/}
                 </Container>
             );
         }
@@ -84,3 +101,42 @@ const styles = StyleSheet.create({
         borderRadius: 5
     }
 });
+
+const cards = [
+    {
+        name: 'Bill Gates',
+        country: 'United States of America',
+        nationalRank: 4,
+        globalRank: 20,
+        totalWorth: '91 Billions',
+        quote: 'If you\'re born poor, it\'s not your mistake. But if you die poor, it\'s your mistake!',
+        image: require('./images/bill_gates.jpg')
+    },
+    {
+        name: 'Jeff Bezos',
+        country: 'United States of America',
+        nationalRank: 7,
+        globalRank: 14,
+        totalWorth: '119.4 Billions',
+        quote: 'If you can\'t tolerate critics, don\'t do anything new or interesting',
+        image: require('./images/bezos.jpg')
+    },
+    {
+        name: 'Jack Ma',
+        country: 'China',
+        nationalRank: 2,
+        globalRank: 9,
+        totalWorth: '39.1 Billions',
+        quote: 'Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine',
+        image: require('./images/jack-ma.jpeg')
+    },
+    {
+        name: 'You can be here too',
+        country: 'Represent your country',
+        nationalRank: 10,
+        globalRank: 10,
+        totalWorth: '200 Billions',
+        quote: 'You can contact all these billionaires and millionaires through this app. Wait for it!',
+        image: require('./images/you.jpg')
+    }
+];
